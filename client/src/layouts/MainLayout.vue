06c569157ebbe0a2671f9c4cd68eb64f5a93fbd0
Eduardo Macias <developer@herwingx.com>
2026-01-08 14:35:36 -0600

    chore(cleanup): eliminar estilos forzados de ConfirmDialog, ahora usamos SweetAlert2

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -113,11 +113,7 @@ onUnmounted(() => {
     </footer>
   </div>
   <Toast />
-  <ConfirmDialog 
-    :pt="{
-      mask: { class: 'bg-black/50 backdrop-blur-sm fixed inset-0 z-50 flex items-center justify-center' }
-    }"
-  />
+  <ConfirmDialog />
 </template>
 
 <style scoped>
